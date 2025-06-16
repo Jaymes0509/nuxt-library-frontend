e8b89e9145d76a8bb061cb9fd835ac7d5fdb3c89
bhuan <bhuang517@gmail.com>
2025-06-16 15:49:42 +0800

    create login pages

---
+++ pages/login.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'blank',
+})
+</script>
+
+<template>
+  <LayoutAuth reverse>
+    <div class="grid mx-auto max-w-sm gap-6">
+      <div class="grid gap-2 text-center">
+        <h1 class="text-2xl font-semibold tracking-tight">
+          會員登入
+        </h1>
+      </div>
+      <AuthSignIn />
+    </div>
+  </LayoutAuth>
+</template>
